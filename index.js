67316e138a3cc88594fb5986432d496c916827ab
Fadee kannah <fadeekannah@gmail.com>
2018-10-30 20:34:27 -0700

    Document and clean up scheduling logic

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const scheduling = require('./util/scheduling');
 parsers.parseCommand().then(input => {
     const output = scheduling.schedule(input);
     const summary = parsers.processOutput(output);
-    console.log(`Total energy Consumed: ${summary.energy}J, idle: ${(summary.idle/input.executionDuration*100).toFixed(2)}% (${summary.idle}/${input.executionDuration}), execution: ${(summary.execution/input.executionDuration*100).toFixed(2)}% (${summary.execution}/${input.executionDuration})`)
+    console.log(`Total energy Consumed: ${summary.energy.toFixed(2)}J, idle: ${(summary.idle/input.executionDuration*100).toFixed(2)}% (${summary.idle}/${input.executionDuration}), execution: ${(summary.execution/input.executionDuration*100).toFixed(2)}% (${summary.execution}/${input.executionDuration})`);
 }).catch(err => {
     if (err.message === 'Missed a deadline') {
-        console.error('Missed a deadline for the task above');
+        parsers.processOutput(err.executionOrder);
+        const task = err.task;
+        console.error(`Missed the deadline of ${task.deadline} for the task ${task.name} that arrived at ${task.arrival} with remaining execution of ${task.remainingExecution}/${task.wcet}`);
     } else {
-        console.error('Unknown error occurred see details below')
-        console.error(err)
+        console.error('Unknown error occurred see details below');
+        console.error(err);
     }
-})
+});
--- util/scheduling.js
+++ util/scheduling.js
@@ -9,11 +9,14 @@ function sortEDF (tasks) {
 }
 
 function schedule({tasks, executionDuration, algorithm, hardware, EE}) {
+    // identify which algorithm is being used
     let EDF = algorithm === 'EDF';
     let sort = EDF ? sortEDF : sortRM;
     let currentTime = 0;
     let readyList = [];
+    // get the hardware to use for each task based on if we are going for energy efficient or not
     const hardwareToUse = power.getHardwareToUse(tasks, hardware, executionDuration, EE);
+    // generate the ready list at time 0
     tasks.forEach((task, index) => {
         readyList.push({
             name: task.name,
@@ -25,20 +28,27 @@ function schedule({tasks, executionDuration, algorithm, hardware, EE}) {
             deadline: currentTime + task.period,
         })
     });
+    // sort the ready list based on the algorithm
     readyList = sort(readyList);
+    // create execution order array used for outputting data
     let executionOrder = [];
     let executingTask = -1;
     while (currentTime < executionDuration) {
+        // check ready list to find which task needs to be executed at the current time
         for (let i = 0; i < readyList.length; i++) {
             let currentTask = readyList[i];
+            // check that the task arrival (ready to be executed)
             if (currentTask.arrival <= currentTime) {
+                // start executing the task
                 executingTask = i;
+                // get last executed task
                 let lastExecutedTask;
                 if (executionOrder.length === 0) {
                     lastExecutedTask = {}
                 } else {
                     lastExecutedTask = executionOrder[executionOrder.length-1];
                 }
+                // if the task is different update the execution order to include the new task
                 if (lastExecutedTask.name !== currentTask.name) {
                     executionOrder.push({
                         start: currentTime,
@@ -50,19 +60,24 @@ function schedule({tasks, executionDuration, algorithm, hardware, EE}) {
             }
         }
 
+        // There is a task being executed at the current time
         if (executingTask > -1) {
             let currentTask = readyList[executingTask];
-            // console.log(currentTime, currentTask.name);
             currentTask.remainingExecution--;
             // task completed
             if (currentTask.remainingExecution === 0) {
+                // update the arrival time, deadline and remaining execution for the task
                 currentTask.arrival += currentTask.period;
                 currentTask.deadline = currentTask.arrival + currentTask.period;
                 currentTask.remainingExecution = currentTask.wcet;
+                // if edf resort the readyList because of the task new deadline
                 if (EDF) readyList = sort(readyList);
+                // finish executing this time
                 executingTask = -1;
             }
         } else {
+            // no task is available to be executed
+            // add idle to the execution order if not there
             let lastExecutedTask = executionOrder[executionOrder.length-1];
             if (lastExecutedTask.name !== 'IDLE') {
                 executionOrder.push({
@@ -72,11 +87,15 @@ function schedule({tasks, executionDuration, algorithm, hardware, EE}) {
                 });
             }
         }
+        // update the end in case the next cycle is a different task/event
         executionOrder[executionOrder.length-1].end = currentTime;
+        // check for missed deadlines
         readyList.forEach((task) => {
             if (task.deadline < currentTime) {
-                console.error(task);
-                throw new Error('Missed a deadline')
+                let err = new Error('Missed a deadline');
+                err.task = task;
+                err.executionOrder = executionOrder;
+                throw err;
             }
         });
         currentTime++;
