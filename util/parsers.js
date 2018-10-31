612a37dd45211c76c4415459b35027c4d63cadd8
Fadee kannah <fadeekannah@gmail.com>
2018-10-30 20:43:09 -0700

    Finalize documentation

--- util/parsers.js
+++ util/parsers.js
@@ -11,14 +11,18 @@ function parseCommand() {
             let hardware = [];
             let tasks = [];
             try {
+                // convert data buffer for the file to a string
                 let fileData = data.toString('utf8');
+                // parse the data to cells where each space is a column and new line a row
                 fileData = fileData.split('\n').map((row) => row.split(' '));
                 let tasksNumber = parseInt(fileData[0][0]);
                 executionDuration = parseInt(fileData[0][1]);
+                // generate hardware object where it links power consumption to its frequency
                 for (let i = 2; i < fileData[0].length; i++) hardware.push({
                     power: parseInt(fileData[0][i]),
                     frequency: hardwareFreq[i-2],
                 });
+                // generate task objects
                 for (let i = 1; i <= tasksNumber; i ++) {
                     let execution = [];
                     for (let j = 2; j < fileData[i].length; j++) execution.push(parseInt(fileData[i][j]));
@@ -31,7 +35,8 @@ function parseCommand() {
             } catch (e) {
                 return reject(new Error('Failed to parse input file'));
             }
-            resolve({
+            // return the parsed command data to the caller function
+            return resolve({
                 executionDuration,
                 algorithm: process.argv[3],
                 EE: process.argv[4] === 'EE',
@@ -43,11 +48,13 @@ function parseCommand() {
 }
 
 function processOutput(data) {
+    // create object to track metrics required
     let output = {
         energy: 0,
         idle: 0,
         execution: 0,
     };
+    // reduce the many records into a single output object
     return data.reduce((aggregated, row) => {
         let execution = row.end - row.start + 1;
         let energyConsumed = row.hardware.power * execution / 1000;
